2c37ab9d2c5bda9630bfbc60c377ace11f0deec3
farsuller <farsuller2020@gmail.com>
2024-11-11 13:56:10 +0800

    added list item material 3

--- app/src/main/java/com/compose/experiment/MainActivity.kt
+++ app/src/main/java/com/compose/experiment/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.compose.experiment
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -10,13 +9,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.compose.experiment.features.pdf_generator.PdfGenerator
+import com.compose.experiment.presentations.list_items.SAMPLE_FRUIT
 import com.compose.experiment.ui.theme.ExperimentLabTheme
 import com.compose.experiment.utils.sharedPreferences
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,8 +50,6 @@ class MainActivity : ComponentActivity() {
 
     private var token by sharedPreferences(name = "token")
 
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -48,11 +58,63 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExperimentLabTheme(dynamicColor = false) {
                 Surface {
-                    Box (
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ){
-                        PdfGenerator()
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize()
+                    ){ innerPadding ->
+                        var fruits by remember { mutableStateOf(SAMPLE_FRUIT) }
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize(),
+                            contentPadding = innerPadding
+                        ) {
+                            items(
+                                items = fruits,
+                                key = { fruit -> fruit.name }){ fruit->
+                                ListItem(
+                                    headlineContent = {
+                                        Text(text = fruit.name)
+                                    },
+                                    supportingContent = {
+                                        Text(text = fruit.description)
+                                    },
+                                    overlineContent = {
+                                        Text(text = fruit.category.name)
+                                    },
+                                    leadingContent = {
+                                        Icon(
+                                            imageVector = Icons.Default.ShoppingCart,
+                                            contentDescription = null,
+                                            tint = fruit.color
+                                        )
+                                    },
+                                    trailingContent = {
+                                        Checkbox(
+                                            checked = fruit.isSelected,
+                                            onCheckedChange = {
+                                                fruits = fruits.map { currentFruit->
+                                                    if (currentFruit == fruit){
+                                                        currentFruit.copy(isSelected = !currentFruit.isSelected)
+                                                    }else{
+                                                        currentFruit
+                                                    }
+                                                }
+                                            }
+                                        )
+                                    },
+                                    modifier = Modifier.fillMaxWidth()
+                                        .clickable {
+                                            fruits = fruits.map { currentFruit->
+                                                if (currentFruit == fruit){
+                                                    currentFruit.copy(isSelected = !currentFruit.isSelected)
+                                                }else{
+                                                    currentFruit
+                                                }
+                                            }
+                                        }
+                                )
+                                HorizontalDivider()
+
+                            }
+                        }
                     }
 
                 }
---
+++ app/src/main/java/com/compose/experiment/presentations/list_items/FruitCategory.kt
@@ -0,0 +1,9 @@
+package com.compose.experiment.presentations.list_items
+
+enum class FruitCategory {
+    BERRY,
+    TROPICAL,
+    CITRUS,
+    DRUPES,
+    MELON
+}
